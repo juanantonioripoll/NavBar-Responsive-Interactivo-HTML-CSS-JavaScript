70d65ed455299ada19203507f612f3aa3aab2a3d
Juan A. Ripoll <129286637+juanantonioripoll@users.noreply.github.com>
2025-06-25 13:42:38 +0200

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,21 @@
+// Selecciona el botón de hamburguesa
+const menuToggle = document.querySelector('.menu-toggle');
 
+// Selecciona el menú móvil
+const mobileMenu = document.querySelector('.mobile-menu');
+
+// Evento para abrir/cerrar el menú móvil
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
+
+// Evento para cerrar el menú al hacer clic fuera
+document.addEventListener('click', (e) => {
+  if (
+    mobileMenu.classList.contains('active') &&
+    !mobileMenu.contains(e.target) &&
+    !menuToggle.contains(e.target)
+  ) {
+    mobileMenu.classList.remove('active');
+  }
+});
